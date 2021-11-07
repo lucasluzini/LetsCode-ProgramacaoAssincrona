5afedada9f9e645949ccb34a710f42af96b465fb
Lucas Luzini <lucasluzini@hotmail.com>
2021-11-06 22:41:15 -0300

    edições finais

--- scripts.js
+++ scripts.js
@@ -1,78 +1,32 @@
-// function fetchFunction (urlForFetch) {
+const btnCarregar = document.querySelector('#btnCarregar');
+const divDados = document.querySelector('#divDados');
+let urlForFetch='https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos/5940/anos/2014-3'
 
-//     const options = {
-//       method: 'GET',
-//     }
+btnCarregar.addEventListener("click", () => {
+    fetchFunction(urlForFetch);
+});
 
-//     return fetch(urlForFetch, options)
-//       .then(response => response.json())
-//     //   .then(console.log)
-//       .catch(err => err)
-//   }
+const preencherDados = (dados) => {
+	if (!dados) {
+		log.textContent = "Não existem registros a serem exibidos.";
+		return;
+	} else {
+		return `
+            ${dados}
+     `;
+	}
+};
 
-//   let varURL='https://parallelum.com.br/fipe/api/v1/carros/marcas';
+function toDo(obj){
+    //console.log(obj)
 
-// //   fetchFunction(varURL)
-// //     .then(() => console.log('cadastrado'))
-// //     .catch(() => console.log('falha ao cadastrar'))
-
-async function requestPost(varResource, dataToSend) {
-    const response = await fetch('https://192.168.0.5' + varResource, {
-        rejectUnauthorized: false,
-        method: "POST",
-        //mode: 'no-cors', //desabilitando o CORS
-        headers: {//ATENÇÃO, O headers tem q vir depois do body para funcionar
-            "Access-Control-Allow-Origin": "*",
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-        },
-  
-        body: JSON.stringify(dataToSend)
-    });
-    const responseData = await response.json();
-  
-    return  responseData;
-  }
-
-
-function handleLoginSubmit(e) {
-    e.preventDefault(); //não deixa a pagina recarregar ao submeter
-
-    (async () => {
-        
-        let dataToSend = {
-            funcao: 'ler',
-            bd_cliente: this.state.bd_cliente,
-            login: this.state.loginUser,
-            senha: this.state.password
-        };
-
-        //console.log("JSON a enviar: " + JSON.stringify(dataToSend));
-
-        let responseData = await requestPost('/usuarios', dataToSend);
-
-        //console.log(responseData);
-        //sconsole.log(responseData.sessao);
-        
-        if (responseData.sessao === 'true'){
-            alert('Usuário Logado!');
-            //ReactDOM.render(<Menu />, document.getElementById('root'));
-            this.setState({ varRedirect: true });
-        }
-        else{
-            alert('Usuário ou senha incorretos!')
-        }
-    
-    })();
-
-    e.target.reset()
+    let objStrint='';
+    Object.getOwnPropertyNames(obj).forEach(function(val, idx, array) {
+        console.log(val + ': ' + obj[val]);
+        objStrint = objStrint + val + ': ' + obj[val] + "<br/>";
+      });
+    divDados.innerHTML = preencherDados(objStrint);
 }
-
-
-
-
-
-let varURL='https://parallelum.com.br/fipe/api/v1/carros/marcas';
 
 function fetchFunction(urlForFetch){
 
@@ -83,15 +37,11 @@ function fetchFunction(urlForFetch){
     return fetch(urlForFetch, options)
     .then((response) => response.json())
     .then((responseData) => {
+        toDo(responseData);
         //console.log(responseData);
-        return responseData;
+        //return responseData;
     })
     .catch(error => console.warn(error));
   }
+
   
-// fetchFunction(varURL).then(response => console.log(response));
-// let varJSONFetchData=fetchFunction(varURL);
-
-let varJSONFetchData = JSON.stringify(fetchFunction(varURL));
-
-console.log(varJSONFetchData);
